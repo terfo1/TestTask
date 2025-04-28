6a2cdaafc297f8b4b7c76237f19abc4f78ed043e
terfo1 <alisher.temirhan@gmail.com>
2025-04-29 03:22:51 +0500

    second commit

--- internal/handler/users.go
+++ internal/handler/users.go
@@ -6,14 +6,61 @@ import (
 	"TestTask/pkg/logger"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
+
+type Pagination struct {
+	Next          int
+	Previous      int
+	RecordPerPage int
+	CurrentPage   int
+	TotalPage     int
+}
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		logger.Logger.Println("Invalid request")
 		http.Error(w, "error", http.StatusBadRequest)
 	}
-	
+	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+	if page < 0 {
+		page = 1
+	}
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	if limit < 0 {
+		limit = 1
+	}
+	age_min := r.URL.Query().Get("age_min")
+	age_max := r.URL.Query().Get("age_max")
+	gender := r.URL.Query().Get("gender")
+	nationality := r.URL.Query().Get("nationality")
+
+	offset := (page - 1) * limit
+	query := database.DB.Model(&models.User{})
+
+	//Filtering
+	if gender != "" {
+		query = query.Where("gender = ?", gender)
+	}
+	if nationality != "" {
+		query = query.Where("nationality = ?", nationality)
+	}
+	if age_min != "" {
+		ageMin, _ := strconv.Atoi(age_min)
+		query = query.Where("age >= ?", ageMin)
+	}
+	if age_max != "" {
+		ageMax, _ := strconv.Atoi(age_max)
+		query = query.Where("age <= ?", ageMax)
+	}
+
+	//Pagination
+	var users []models.User
+	res := query.Limit(limit).Offset(offset).Find(&users)
+	if res.Error != nil {
+		logger.Logger.Println("Pagination failed")
+		http.Error(w, "error", http.StatusInternalServerError)
+	}
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -38,4 +85,5 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	logger.Logger.Println("User created successfully!")
+	w.WriteHeader(http.StatusCreated)
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,5 +9,8 @@ func SetupRoutes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/createuser", handler.CreateUser)
 	mux.HandleFunc("/user", handler.GetUsers)
+	// Not implemented yet
+	// mux.HandleFunc("/user", handler.UpdateUsers)
+	// mux.HandleFunc("/user", handler.DeleteUsers)
 	return mux
 }
